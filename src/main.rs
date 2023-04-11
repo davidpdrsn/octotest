662850d53ff38e25183f3ab84edf87b519379ba2
David Pedersen <david.pdrsn@gmail.com>
2023-04-11 12:34:26 +0200

    a pr

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,1 @@
-#![allow(dead_code)]
-
-fn scope(f: impl FnOnce()) {
-    f();
-}
-
-fn foo(iter: impl Iterator<Item = u32>) {
-    // A and B.
-    scope(move || for _i in iter {});
-}
-
-fn main() {
-    println!("hello world!");
-}
+fn main() {}
